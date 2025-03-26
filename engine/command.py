6400afe7b38c31cf0cb81f02834cfabdf315f197
Shubham Patidar <shubhampatidar2812@gmail.com>
2025-03-26 10:51:12 +0530

    Full Project

---
+++ engine/command.py
@@ -0,0 +1,91 @@
+import time
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install SpeechRecognition
+import eel
+
+
+def speak(text):
+    text = str(text)
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices') 
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 190)
+    eel.DisplayMessage(text)
+    engine.say(text)
+    eel.receiverText(text)
+    engine.runAndWait()
+
+@eel.expose
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        eel.DisplayMessage('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        eel.DisplayMessage('recognizing....')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        eel.DisplayMessage(query)
+        time.sleep(2)
+        #speak(query)
+    
+       
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+@eel.expose
+def allCommands(message=1):
+
+    if message == 1:
+        query = takecommand()
+        print(query)
+        eel.senderText(query)
+    else:
+        query = message
+        eel.senderText(query)
+    try:
+        # query = takecommand()
+        # print(query)
+        if "open" in query:
+            from engine.features import openCommand
+            openCommand(query)
+        elif "on Youtube" in query:
+            from engine.features import PlayYoutube
+            PlayYoutube(query)
+        
+        elif "send message" in query or "phone call" in query or "video call" in query:
+            from engine.features import findContact, whatsApp
+            flag = ""
+            contact_no, name =findContact(query)
+            if(contact_no != 0):
+
+                if "send message" in query:
+                    flag = 'message'
+                    speak("what message to send")
+                    query = takecommand()
+                    
+                elif "phone call" in query:
+                    flag = 'call'
+                else:
+                    flag = 'video call'
+                    
+                whatsApp(contact_no, query, flag, name)
+            
+
+        else:
+            from engine.features import chatBot
+            chatBot(query)
+    except:
+        print("error")
+    
+    eel.ShowHood()
